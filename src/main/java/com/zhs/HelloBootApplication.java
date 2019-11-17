aabc27680a75d4d1eb3fe458f715b6a450595321
ZHSYQ <2534798858@qq.com>
2019-11-17 12:18:13 +0800

    https证书认证

--- src/main/java/com/zhs/HelloBootApplication.java
+++ src/main/java/com/zhs/HelloBootApplication.java
@@ -2,11 +2,15 @@ package com.zhs;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.zhs.utils.SnowflakeIdWorker;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 
+import javax.naming.Context;
 import javax.persistence.EntityManager;
 
 @SpringBootApplication
@@ -31,4 +35,41 @@ public class HelloBootApplication {
     public JPAQueryFactory jpaQueryFactory(EntityManager entityManager){
         return new JPAQueryFactory(entityManager);
     }
+
+//    //拦截所有请求
+//    @Bean
+//    public EmbeddedServletContainerFactory servletContainer() {
+//        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
+//            @Override
+//            protected void postProcessContext(Context context) {
+//                SecurityConstraint constraint = new SecurityConstraint();
+//                constraint.setUserConstraint("CONFIDENTIAL");
+//                SecurityCollection collection = new SecurityCollection();
+//                collection.addPattern("/*");
+//                constraint.addCollection(collection);
+//                context.addConstraint(constraint);
+//            }
+//        };
+//        tomcat.addAdditionalTomcatConnectors(httpConnector());
+//        return tomcat;
+//    }
+//
+//    //配置http转https
+//    @Bean
+//    public Connector httpConnector() {
+//        Connector connector = new Connector(TomcatEmbeddedServletContainerFactory.DEFAULT_PROTOCOL);
+//        connector.setScheme("http");
+//        //Connector监听的http的端口号
+//        connector.setPort(80);
+//        connector.setSecure(false);
+//        //监听到http的端口号后转向到的https的端口号
+//        connector.setRedirectPort(443);
+//        return connector;
+//    }
+//
+//    //这里设置默认端口为443，即https的，如果这里不设置，会https和http争夺80端口
+//    @Override
+//    public void customize(ConfigurableEmbeddedServletContainer container) {
+//        container.setPort(443);
+//    }
 }
